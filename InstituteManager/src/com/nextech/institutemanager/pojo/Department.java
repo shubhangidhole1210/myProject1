73766e70476ddfd91cbf8bdf4a8de0bfe8e64613
Shahadeo Chavan <shahadeochavan@gmail.com>
2016-05-04 16:11:53 +0530

    I remove get and set methods.
    from institude manger

--- InstituteManager/src/com/nextech/institutemanager/pojo/Department.java
+++ InstituteManager/src/com/nextech/institutemanager/pojo/Department.java
@@ -84,24 +84,6 @@ public class Department {
 	        }
 	}
 
-	public String getName() {
-		return name;
-	}
-	public void setName(String name) {
-		this.name = name;
-	}
-	public int getId() {
-		return id;
-	}
-	public void setId(int id) {
-		this.id = id;
-	}
-	public String getDescription() {
-		return description;
-	}
-	public void setDescription(String description) {
-		this.description = description;
-	}
 
 
 
